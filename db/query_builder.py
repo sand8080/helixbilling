4c77bf15e3176306fee5d4f59ced2fec7092cba0
Alexander Kislitsky <sand8080@gmail.com>
2009-07-30 01:07:18 +0400

    update, insert, delete requests implemented.

--- db/query_builder.py
+++ db/query_builder.py
@@ -1,73 +1,19 @@
-def quote(val):
-    q, s = '"', '.'
-    return  s.join([x.startswith(q) and x.endswith(q) and x or '"%s"' % x for x in val.split(s)])
+from cond import quote, quote_list, quote_value
 
-def quote_list(lst, separator=','):
-    return separator.join(map(quote, lst))
-
-def quote_value(val, quote="'"):
-    if isinstance(val, str) and not (val.startswith(quote) and val.endswith(quote)):
-        return "'%s'" % val
-    else:
-        return val
-
-def _triple_expr(lh, oper, rh):
-    return '%s %s %s' % (lh, oper, rh)
-
-class Cond(object):
-    def __init__(self, lh, oper, rh):
-        self.lh = lh
-        self.oper = oper
-        self.rh = rh
-    def glue(self):
-        return _triple_expr(
-            quote(self.lh),
-            self.oper,
-            quote_value(self.rh)
-        )
-
-class EqCond(Cond):
-    def __init__(self, lh, rh):
-        super(EqCond, self).__init__(lh, '=', rh)
-
-class NotCond(object):
-    def __init__(self, lh, rh):
-        self.lh = lh
-        self.rh = rh
-    def glue(self):
-        return _triple_expr(quote(self.lh), 'NOT', self.rh)
-    
-class ScopedCond(object):
-    def __init__(self, cond):
-        self.cond = cond
-    def glue(self):
-        return '(%s)' % self.cond.glue()
-
-class AndCond(object):
-    def __init__(self, lh, rh):
-        self.lh = lh
-        self.rh = rh
-    def glue(self):
-        return _triple_expr(self.lh.glue(), 'AND', self.rh.glue())
-
-class OrCond(object):
-    def __init__(self, lh, rh):
-        self.lh = lh
-        self.rh = rh
-    def glue(self):
-        return _triple_expr(self.lh.glue(), 'OR', self.rh.glue())
+def __where(cond):
+    return '' if cond is None else 'WHERE %s' % cond.glue()
 
 def select(table, columns=None, cond=None, group_by=None, order_by=None, limit=0, offset=0, for_update=False):
     tpl = {}
     tpl['target']   = '*' if columns == None else quote_list(columns)
     tpl['table']    = quote(table)
-    tpl['where']    = ''  if cond is None else 'WHERE %s' % cond.glue()
+    tpl['where']    = __where(cond)
     tpl['group_by'] = ''  if group_by == None else 'GROUP BY %s' % quote_list(group_by)
     tpl['limit']    = ''  if limit == 0 else 'LIMIT %d' % limit
     tpl['offset']   = ''  if offset == 0 else 'OFFSET %d' % offset
     tpl['order_by'] = ''
     tpl['locking']  = ''  if not for_update else 'FOR UPDATE'
-    
+
     if order_by != None:
         if order_by[0] == '-':
             tpl['order_by'] = 'ORDER BY %s DESC' % quote_list(order_by[1:])
@@ -78,39 +24,23 @@ def select(table, columns=None, cond=None, group_by=None, order_by=None, limit=0
         'SELECT %(target)s FROM %(table)s %(where)s %(group_by)s %(order_by)s %(limit)s %(offset)s %(locking)s' % tpl
     ).strip()
 
-#    if columns:
-#        parts.append(", ".join( escape(column) for column in columns ))
-#    else:
-#        parts.append("*")
-#    parts.append("FROM")
-#    parts.append(select_from)
-#
-#    if join:
-#        join_sql, join_params = join
-#        parts.append(join_sql)
-#        params.extend(join_params)
-#    if conditions:
-#        where_query, where_params = where(**conditions)
-#        parts.append(where_query)
-#        params += where_params
-#    if group_by:
-#        group_parts = []
-#        parts.append("GROUP BY")
-#        for col in group_by:
-#            group_parts.append(escape(col))
-#        parts.append(", ".join(group_parts))
-#    if order_by:
-#        order_parts = []
-#        parts.append("ORDER BY")
-#        for col in order_by:
-#            if col[0] == "-":
-#                order_parts.append(escape(col[1:]) + " DESC")
-#            else:
-#                order_parts.append(escape(col) + " ASC")
-#        parts.append(", ".join(order_parts))
-#    if limit:
-#        parts.append("LIMIT %d" % limit)
-#    if offset:
-#        parts.append("OFFSET %d" % offset)
-#    query = " ".join(parts) + ";"
-#    return (query, params)
+def update(table, updates, cond):
+    tpl = {}
+    tpl['table']    = quote(table)
+    tpl['where']    = __where(cond)
+    tpl['updates']  = ','.join(['%s = %s' % (quote(k), quote_value(v)) for (k, v) in updates.iteritems()])
+    return ('UPDATE %(table)s SET %(updates)s %(where)s' % tpl).strip()
+
+def delete(table, cond):
+    tpl = {}
+    tpl['table']    = quote(table)
+    tpl['where']    = __where(cond)
+    return ('DELETE FROM %(table)s %(where)s' % tpl).strip()
+
+def insert(table, inserts):
+    tpl = {}
+    tpl['table']    = quote(table)
+    columns = inserts.keys()
+    tpl['columns']  = ','.join(map(quote, columns))
+    tpl['values']   = ','.join(map(lambda k: '%s' % quote_value(inserts[k]), columns))
+    return ('INSERT INTO %(table)s (%(columns)s) VALUES (%(values)s)' % tpl).strip()
--- db/test_query_builder.py
+++ db/test_query_builder.py
@@ -1,64 +1,9 @@
-# -*- coding: utf-8 -*-
 import unittest
 
-from db.query_builder import quote, quote_value, quote_list
-from db.query_builder import Cond, EqCond, NotCond, AndCond, OrCond, ScopedCond
-from db.query_builder import select
+from db.cond import AndCond, Cond, EqCond
+from db.query_builder import select, update, delete, insert
 
-class QueryBuilder(unittest.TestCase):
-    def test_quote(self):
-        self.assertEqual('"id"', quote('id'))
-        self.assertEqual('"id"', quote('"id"'))
-        self.assertEqual('""id"', quote('"id'))
-        self.assertEqual('"id""', quote('id"'))
-        self.assertEqual('"billing"."id"', quote('billing.id'))
-        self.assertEqual('"billing"."id"', quote('"billing"."id"'))
-        self.assertEqual('""billing"."id""', quote('"billing.id"'))
-        self.assertEqual('"billing"."id"."cd"', quote('billing.id.cd'))
-    
-    def test_quote_value(self):
-        self.assertEqual(10, quote_value(10))
-        self.assertEqual("'some string'", quote_value("some string"))
-        self.assertEqual("'some string'", quote_value("'some string'"))
-        self.assertEqual('\'\'some string\'', quote_value("'some string"))
-        self.assertEqual('\'some string\'\'', quote_value("some string'"))
-        self.assertEqual("'D\'Artanian is like possum'", quote_value('D\'Artanian is like possum'))
-        
-    def test_quote_list(self):
-        self.assertEqual(('"1"."1","1"."2","1"."2"."1"'), quote_list(('1.1', '1.2', '1.2.1')))
-        
-    def test_cond(self):
-        self.assertEqual('"billing"."id" < 0', Cond('billing.id', '<', 0).glue())
-        test_str = 'хитрый cat'
-        self.assertEqual('"billing"."id" != \'%s\'' % test_str, Cond('billing.id', '!=', test_str).glue())
-        
-    def test_eq_cond(self):
-        cond_end = EqCond('billing.id', 0)
-        self.assertEqual('"billing"."id" = 0', cond_end.glue())
-        
-    def test_not_cond(self):
-        cond_end = NotCond('billing.id', 'NULL')
-        self.assertEqual('"billing"."id" NOT NULL', cond_end.glue())
-        
-    def test_and_cond(self):
-        cond_lh = Cond('billing.id', '=', 0)
-        cond_rh = Cond('billing.cd', '!=', 1)
-        cond_end = AndCond(cond_lh, cond_rh)
-        self.assertEqual('"billing"."id" = 0 AND "billing"."cd" != 1', cond_end.glue())
-        
-    def test_or_cond(self):
-        cond_lh = Cond('billing.id', '=', 0)
-        cond_rh = Cond('billing.cd', '!=', 1)
-        cond_end = OrCond(cond_lh, cond_rh)
-        self.assertEqual('"billing"."id" = 0 OR "billing"."cd" != 1', cond_end.glue())
-        
-    def test_scoped_cond(self):
-        cond_lh = Cond('billing.id', '=', 'a')
-        cond_rh = Cond('billing.cd', '!=', 'b')
-        cond_and = AndCond(cond_lh, cond_rh)
-        cond_end = ScopedCond(cond_and)
-        self.assertEqual('("billing"."id" = \'a\' AND "billing"."cd" != \'b\')', cond_end.glue())
-        
+class QueryBuilderTestCase(unittest.TestCase):
     def test_select(self):
         self.assertEqual('SELECT * FROM "billing"', select('billing'))
         self.assertEqual(
@@ -85,7 +30,7 @@ class QueryBuilder(unittest.TestCase):
             'SELECT * FROM "billing"   ORDER BY "id","ammount" ASC LIMIT 5 OFFSET 6',
             select('billing', order_by=['id', 'ammount'], limit=5, offset=6)
         )
-        
+
         cond_and = AndCond(
             Cond('billing.amount', '>', 10),
             Cond('billing.amount', '<', 100)
@@ -98,6 +43,24 @@ class QueryBuilder(unittest.TestCase):
             'SELECT * FROM "billing" WHERE "id" = 5     FOR UPDATE',
             select('billing', cond=EqCond('id', 5), for_update=True)
         )
-         
+
+    def test_update(self):
+        self.assertEqual(
+            'UPDATE "balance" SET "currency" = \'rur\',"client_id" = 4 WHERE "id" = 1',
+            update('balance', {'client_id': 4, 'currency': 'rur'}, EqCond('id', 1))
+        )
+
+    def test_delete(self):
+        self.assertEqual(
+            'DELETE FROM "balance_lock" WHERE "balance_id" = 7',
+            delete('balance_lock', EqCond('balance_id', 7))
+        )
+
+    def test_insert(self):
+        self.assertEqual(
+            'INSERT INTO "balance" ("currency","amount","client_id") VALUES (\'usd\',0,42)',
+            insert('balance', {'client_id': 42, 'amount': 0, 'currency': 'usd'})
+        )
+
 if __name__ == '__main__':
     unittest.main()
