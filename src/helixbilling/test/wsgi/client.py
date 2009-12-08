44b69efa535ee9dbff439dc93f74f1df97a96f98
Alexander Kislitsky <sand8080@gmail.com>
2009-12-08 13:58:57 +0300

    Https protocol handled in client test applications.

--- src/helixbilling/test/wsgi/client.py
+++ src/helixbilling/test/wsgi/client.py
@@ -2,8 +2,8 @@ from helixcore.test.util import ClientApplication
 
 
 class Client(ClientApplication):
-    def __init__(self, host, port, login, password):
-        super(Client, self).__init__(host, port, login, password)
+    def __init__(self, host, port, login, password, protocol='http'):
+        super(Client, self).__init__(host, port, login, password, protocol=protocol)
 
     def ping(self):
         return self.request({'action': 'ping'})
--- src/helixbilling/wsgi/server.py
+++ src/helixbilling/wsgi/server.py
@@ -10,7 +10,7 @@ from helixbilling.validator.validator import api_scheme
 class Server(object):
     class ServerLog(object):
         def write(self, s, l=0): #IGNORE:W0613
-            logger.info('server: %s' % s)
+            logger.debug('server: %s' % s)
 
     @staticmethod
     def run():
