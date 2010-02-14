044a10f8ab05a90096fab9592ccdf6593c590209
Alexander Kislitsky <sand8080@gmail.com>
2010-02-14 17:24:46 +0300

    Mistype fixed.

--- src/helixbilling/conf/db.py
+++ src/helixbilling/conf/db.py
@@ -3,7 +3,7 @@ from functools import partial
 import psycopg2.extensions
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 
-import helixtariff.conf.lock_order #@UnusedImport IGNORE:W0611
+import helixbilling.conf.lock_order #@UnusedImport IGNORE:W0611
 import helixcore.db.wrapper as wrapper
 from settings import DSN
 
