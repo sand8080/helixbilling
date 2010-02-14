b51116d68995255db6bb5196937d526c1f3aaa17
Alexander Kislitsky <sand8080@gmail.com>
2010-02-14 17:25:19 +0300

    Error BalanceDisabled added.

--- src/helixbilling/error.py
+++ src/helixbilling/error.py
@@ -14,6 +14,11 @@ class BalanceNotFound(ObjectNotFound):
         super(BalanceNotFound, self).__init__('Balance not found for customer %s' % customer_id)
 
 
+class BalanceDisabled(ObjectNotFound):
+    def __init__(self, customer_id):
+        super(BalanceDisabled, self).__init__('Balance disabled for customer %s' % customer_id)
+
+
 class CurrencyNotFound(ObjectNotFound):
     def __init__(self, currency):
         super(CurrencyNotFound, self).__init__('Currency %s not found' % currency)
